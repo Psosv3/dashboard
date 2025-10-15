69e2e34729d52268755e7e26671f90de37136728
Psosv3 <contact@onexus.info>
2025-10-15 21:56:11 +0200

    commit_for_merge

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f38221f50cf1ffa4"; }
+/******/ 		__webpack_require__.h = function() { return "1217128fd18a1417"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
